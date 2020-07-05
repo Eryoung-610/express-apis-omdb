04384853dc5d5c047578649e78ab9c9886b891e8
Eryoung-610 <eryoung610@gmail.com>
2020-07-05 02:07:07 -0700

    Fixed routing, display team names now

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios')
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 
@@ -17,8 +18,41 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('home');
 });
+
+app.get('/results', (req,res) => {
+  let title = req.query.title;
+  axios.get(`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=` + title)
+  .then(response => {
+    let result = response.data.Search
+    let resultData = {
+      title: result.Title,
+      year: result.Year,
+      poster: result.Poster,
+      imdbID: result.imdbID
+    }
+
+    res.render('results', {result})
+    // console.log(movies.length)
+  }).catch(err => {
+    res.send(err)
+  })
+})
+
+app.get('/detail/:movie_id', (req,res) => {
+  let id = req.params.movie_id
+  axios.get(`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&i=` + id).then (response => {
+    let result = response.data
+    let resultData = {
+      title: result.Title,
+      year: result.Year,
+      poster: result.Poster,
+      imdbID: result.imdbID
+    }
+    res.render('detail', {resultData})
+  })
+})
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
